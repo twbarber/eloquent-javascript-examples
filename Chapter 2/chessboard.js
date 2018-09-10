b2ccdffb45a568cd633d14abbf70d64812ee304b
Tyler Barber <dev@twbarber.com>
2018-09-10 08:22:43 -0400

    Rename to chessboard.

--- Chapter 2/chessboard.js
+++ Chapter 2/chessboard.js
@@ -4,7 +4,7 @@ function chessboard(size) {
     let rowBase = buildRow(size);
     for(let i = 0; i < size; i++) {
         let row = i % 2 === 0 ? " " + rowBase : rowBase + (size % 2 === 0 ? " " : "#");
-        console.log(row)
+        console.log(row);
     }
 }
 
@@ -12,7 +12,7 @@ function buildRow(size) {
     let squares = Math.floor(size/2);
     let base = "";
     for(let i = 0; i < squares; i++) {
-        base += "# "
+        base += "# ";
     }
-    return size % 2 === 0 ? base.trim() : base
+    return size % 2 === 0 ? base.trim() : base;
 }
